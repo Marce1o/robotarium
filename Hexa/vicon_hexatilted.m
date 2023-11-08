6be22740652f1b95900c545e3da614fa839a47a2
Daniel Erro <danielerrog@gmail.com>
2023-11-07 21:17:31 -0600

    refactor(vicon_hexatilted_read): using quaternions for relative orientation

--- Hexa/vicon_hexatilted.m
+++ Hexa/vicon_hexatilted.m
@@ -5,10 +5,10 @@ clear, clc
 % 
 % rosinit 
 
-sigmanoise = 0.00000001;
+sigmanoise = 0.00000005;
 
 tf = 3;
 open_system('vicon_hexatilted_read')
-sim("vicon_hexatilted_read.slx")
+% sim("vicon_hexatilted_read.slx")
 
 % rosshutdown
