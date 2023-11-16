3007e61b030d74b2cfb646947be91f12f4309901
CarlosCharbel <110117210+CarlosCharbel@users.noreply.github.com>
2023-11-16 16:39:32 -0600

    Check gimbal controller

--- example_codes/move_gimbal.py
+++ example_codes/move_gimbal.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     # slp =3
     # # 控制云台向右以30度每秒旋转3秒
-    ep_gimbal.drive_speed(pitch_speed=0, yaw_speed=00)
+    ep_gimbal.drive_speed(pitch_speed=0, yaw_speed=0)
     time.sleep(3)
 
     # # 控制云台向左以30度每秒旋转3秒
