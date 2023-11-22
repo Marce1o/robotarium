3690e416c3ded7799d02c4bd70a3ddc3dcca373e
Daniel Erro <danielerrog@gmail.com>
2023-11-21 18:48:23 -0600

    real time pwm

--- Hexa/vicon_hexatilted.m
+++ Hexa/vicon_hexatilted.m
@@ -1,12 +1,31 @@
 %dbstop if error
 clear, clc
 
-rosshutdown 
-close_system('vicon_hexatilted_read')
-rosinit 
-
-sigmanoise = 0.0000001;
+% rosshutdown 
+% close_system('vicon_hexatilted_read')
+% rosinit 
+% setenv('ROS_MASTER_URI', 'http://192.168.0.145:11311')
 
 tf = Inf;
-open_system('vicon_hexatilted_read')
-sim("vicon_hexatilted_read.slx")
+mdl = "send_pwm.slx";
+open_system(mdl)
+% sim(mdl)
+
+%% Before connecting ESP
+
+% rosnode list
+% /matlab_global_node_27250
+% /rosout
+% /send_pwm_90580
+% /vicon
+
+% rostopic list
+% /PWM                  
+% /diagnostics          
+% /rosout               
+% /rosout_agg           
+% /tf                   
+% /vicon/QuadGus/QuadGus
+% /vicon/markers    
+
+
